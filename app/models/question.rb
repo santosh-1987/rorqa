eec5c48583101d7ab7ef5d6e3a96e0390d8a0110
Santosh Kumar Mohanty <santa.jyp@gmail.com>
2017-05-17 17:17:18 +0530

    Added Tag Services

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :dashboard
   has_many :answers,dependent: :destroy
+  has_many :tagged_services
+  has_many :tags, through: :tagged_services
   validates :question, :presence => true
 end
