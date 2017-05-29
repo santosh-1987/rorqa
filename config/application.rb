cbe16cdb013cae4983ed9ca7075ccbbd0a3a740d
Dassmruti <smruti.sagarika06@gmail.com>
2017-05-29 15:30:19 +0530

    Removed Caching

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Rorqa
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.action_controller.page_cache_directory =  File.join(Rails.root, 'public', 'cache')
+    # config.action_controller.page_cache_directory =  File.join(Rails.root, 'public', 'cache')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
