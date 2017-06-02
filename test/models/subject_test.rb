8789d999c14ba71574df96f18a570babfeb67cd8
Santosh Kumar Mohanty <santa.jyp@gmail.com>
2017-06-02 15:43:04 +0530

    Added Unit Test cases for Subject Model

--- test/models/subject_test.rb
+++ test/models/subject_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :subjects
+  test "should not save subject without name" do
+    subject = Subject.new
+    assert_not subject.save
+  end
+
+  test "should save subject name" do
+    subject = Subject.new(:name => "Ruby").save
+    assert_equal true, subject
+  end
+
+  test "Retreive from Fixture" do
+    assert_equal "Rails", subjects(:one).name
+    assert_equal "Maths", subjects(:two).name
+    assert_not_empty(subjects(:one).name)
+  end
+
 end
